c7b228a0d2e3b848732c246c3f9c0b0dc34a5019
YanaKudrinskaya <yanakudrinskaya86@gmail.com>
2025-08-30 08:48:15 +0300

    Добавила SharedPreferences и Gson

--- app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
@@ -1,6 +1,17 @@
 package ru.practicum.android.diploma.di
 
+import android.content.Context
+import com.google.gson.Gson
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
+import ru.practicum.android.diploma.data.EXAMPLE_PREFERENCES
 
 val dataModule = module {
+
+    single {
+        androidContext()
+            .getSharedPreferences(EXAMPLE_PREFERENCES, Context.MODE_PRIVATE)
+    }
+
+    factory { Gson() }
 }
