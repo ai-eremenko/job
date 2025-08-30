ccb8a0df6d7ca4b3f2e79142847675ddc31870f7
YanaKudrinskaya <yanakudrinskaya86@gmail.com>
2025-08-30 08:37:23 +0300

    Добавила библиотеки Gson и Koin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,12 @@ android {
 }
 
 dependencies {
+    // Gson
+    implementation(libs.gson)
+
+    //Koin
+    implementation(libs.io)
+
     implementation(libs.androidX.core)
     implementation(libs.androidX.appCompat)
 
