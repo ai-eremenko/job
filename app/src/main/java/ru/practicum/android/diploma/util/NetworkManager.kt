2290a659e870c7feac5da64591d48896300af08d
YanaKudrinskaya <yanakudrinskaya86@gmail.com>
2025-09-01 11:22:50 +0300

    fix

--- app/src/main/java/ru/practicum/android/diploma/util/NetworkManager.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/NetworkManager.kt
@@ -9,13 +9,10 @@ class NetworkManager(private val context: Context) {
     fun isConnected(): Boolean {
         val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         val capabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
-        if (capabilities != null) {
-            when {
-                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> return true
-                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> return true
-                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> return true
-            }
-        }
-        return false
+        return capabilities?.let {
+            it.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
+                it.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+                it.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)
+        } ?: false
     }
 }
