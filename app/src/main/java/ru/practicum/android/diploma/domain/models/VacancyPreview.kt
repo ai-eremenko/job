e442a93ac418d36dfc90111594d702bd46f66fad
Angelina Eremenko <dolgikhanig@mail.ru>
2025-09-03 23:24:11 +0300

    GH-18. Верстка item для RecyclerView, создание VacanciListAdapter и VacanciListViewHolder

---
+++ app/src/main/java/ru/practicum/android/diploma/domain/models/VacancyPreview.kt
@@ -0,0 +1,12 @@
+package ru.practicum.android.diploma.domain.models
+
+data class VacancyPreview(
+    val id: String,
+    val name: String,
+    val albumCoverUrl: String?,
+    val salaryCurrency: String?,
+    val salaryFrom: String?,
+    val salaryTo: String?,
+    val addressCity: String?,
+    val employerName: String
+)
---
+++ app/src/main/java/ru/practicum/android/diploma/ui/search/adapter/VacancyListViewHolder.kt
@@ -0,0 +1,45 @@
+package ru.practicum.android.diploma.ui.search.adapter
+
+import android.content.Context
+import android.util.TypedValue
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.databinding.VacancyItemBinding
+import ru.practicum.android.diploma.domain.models.VacancyPreview
+
+class VacancyListViewHolder (
+    private val binding: VacancyItemBinding,
+    private val onClick: (VacancyPreview) -> Unit,
+) : RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(model: VacancyPreview) {
+        binding.root.setOnClickListener {
+            onClick(model)
+        }
+        binding.name.text = "${model.name}, ${model.addressCity}"
+        binding.employerName.text = model.employerName
+        binding.salary.text = when {
+            model.salaryFrom.isNullOrEmpty().not() && model.salaryTo.isNullOrEmpty().not() -> "От ${model.salaryFrom} до  ${model.salaryTo}"
+            model.salaryFrom.isNullOrEmpty().not() -> "От ${model.salaryFrom}"
+            model.salaryCurrency.isNullOrEmpty().not() -> model.salaryCurrency
+            else -> "Зарплата не указана"
+        }
+        Glide.with( binding.root)
+            .load(model.albumCoverUrl)
+            .placeholder(R.drawable.placeholder_vacancy_preview)
+            .transform(RoundedCorners(itemView.context.toPx(12)))
+            .diskCacheStrategy(DiskCacheStrategy.NONE)
+            .skipMemoryCache(true)
+            .into( binding.sourceImage)
+    }
+
+    private fun Context.toPx(dp: Int): Int =
+        TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            dp.toFloat(),
+            resources.displayMetrics
+        ).toInt()
+}
