a2bdc7222fc516284626bd3e6fb65fb33b0b2b27
Angelina Eremenko <dolgikhanig@mail.ru>
2025-09-05 12:53:31 +0300

    Перенесла  FavoriteInteractorImpl в папку domain.favorite

--- app/src/main/java/ru/practicum/android/diploma/domain/favorite/FavoriteInteractorImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/favorite/FavoriteInteractorImpl.kt
@@ -1,8 +1,6 @@
-package ru.practicum.android.diploma.domain.impl
+package ru.practicum.android.diploma.domain.favorite
 
 import kotlinx.coroutines.flow.Flow
-import ru.practicum.android.diploma.domain.favorite.FavoriteInteractor
-import ru.practicum.android.diploma.domain.favorite.FavoriteRepository
 import ru.practicum.android.diploma.domain.vacancy.models.Vacancy
 
 class FavoriteInteractorImpl(
