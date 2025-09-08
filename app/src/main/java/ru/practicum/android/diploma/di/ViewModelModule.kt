e19010749578d123015a18a3b11b4d8f33a8c981
YanaKudrinskaya <yanakudrinskaya86@gmail.com>
2025-09-08 11:06:59 +0300

    fix detect

--- app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
@@ -1,10 +1,10 @@
 package ru.practicum.android.diploma.di
 
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.module.dsl.viewModelOf
-import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
-import ru.practicum.android.diploma.presentation.vacancy.VacancyViewModel
 import ru.practicum.android.diploma.presentation.favorite.FavoriteViewModel
+import ru.practicum.android.diploma.presentation.vacancy.VacancyViewModel
 
 val viewModelModule = module {
 
