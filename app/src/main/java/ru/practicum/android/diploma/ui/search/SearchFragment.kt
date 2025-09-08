3a6060e1d7165ab75b8397a276f1c706e39a749d
ilsdan <me@ilsdan.ru>
2025-09-08 11:06:37 +0200

    Реализован переход на страницу Vacancy

--- app/src/main/java/ru/practicum/android/diploma/ui/search/SearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/search/SearchFragment.kt
@@ -6,10 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
+import androidx.core.os.bundleOf
 import androidx.core.view.isVisible
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentSearchBinding
@@ -71,7 +73,8 @@ class SearchFragment : Fragment() {
     }
 
     private fun openVacancy(vacancy: VacancyPreviewPresent) {
-        TODO()
+        val bundle = bundleOf("vacancy_id" to vacancy.id)
+        findNavController().navigate(R.id.action_searchFragment_to_vacancyFragment, bundle)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
