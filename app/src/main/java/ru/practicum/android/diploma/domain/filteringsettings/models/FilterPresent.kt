08fe9c739f4701f1af1d79ec29301698750008bf
YanaKudrinskaya <yanakudrinskaya86@gmail.com>
2025-09-11 16:55:40 +0300

    Объединила FilterSettings с FilterPresent.kt чтобы при отсутствии интернета страница фильтрации корректно подгружала названия выбранных фильтров

--- app/src/main/java/ru/practicum/android/diploma/domain/filteringsettings/models/FilterPresent.kt
+++
@@ -1,8 +0,0 @@
-package ru.practicum.android.diploma.domain.filteringsettings.models
-
-data class FilterPresent(
-    val area: String?,
-    val industry: String?,
-    val salary: Int?,
-    val onlyWithSalary: Boolean
-)
