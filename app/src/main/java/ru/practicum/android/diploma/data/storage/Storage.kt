a80ff658e57869ff547f69caf846417d2f32b225
YanaKudrinskaya <yanakudrinskaya86@gmail.com>
2025-09-12 07:45:14 +0300

    Изменила на сохранение null если все настройки фильтра сброшены.

--- app/src/main/java/ru/practicum/android/diploma/data/storage/Storage.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/storage/Storage.kt
@@ -3,6 +3,7 @@ package ru.practicum.android.diploma.data.storage
 import android.content.SharedPreferences
 import com.google.gson.Gson
 import ru.practicum.android.diploma.domain.filteringsettings.models.FilterSettings
+import androidx.core.content.edit
 
 const val EXAMPLE_PREFERENCES = "example_preferences"
 const val FILTERS = "filters"
@@ -11,19 +12,24 @@ class Storage(
     val sharedPreferences: SharedPreferences,
     private val gson: Gson,
 ) {
-    fun getFilterOptions(): FilterSettings {
-        val json = sharedPreferences.getString(FILTERS, null) ?: return FilterSettings()
+    fun getFilterOptions(): FilterSettings? {
+        val json = sharedPreferences.getString(FILTERS, null) ?: return null
         return gson.fromJson(json, FilterSettings::class.java)
     }
 
-    fun saveFilterOptions(filter: FilterSettings) {
-        sharedPreferences.edit()
-            .putString(FILTERS, createJsonFromFilter(filter))
-            .apply()
+    fun saveFilterOptions(filter: FilterSettings?) {
+        if (filter == null) {
+            sharedPreferences.edit {
+                putString(FILTERS, null)
+            }
+        } else {
+            sharedPreferences.edit {
+                putString(FILTERS, createJsonFromFilter(filter))
+            }
+        }
     }
 
-    private fun createJsonFromFilter(filter: FilterSettings): String {
+    private fun createJsonFromFilter(filter: FilterSettings?): String? {
         return gson.toJson(filter)
     }
 }
-
