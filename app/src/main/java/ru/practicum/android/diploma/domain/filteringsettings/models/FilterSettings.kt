419c470837517ee12b1ffccffd3cec9fe73d2858
YanaKudrinskaya <yanakudrinskaya86@gmail.com>
2025-09-12 07:37:48 +0300

    Убрала FilterPresent.kt

--- app/src/main/java/ru/practicum/android/diploma/domain/filteringsettings/models/FilterSettings.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/filteringsettings/models/FilterSettings.kt
@@ -2,7 +2,9 @@ package ru.practicum.android.diploma.domain.filteringsettings.models
 
 data class FilterSettings(
     val areaId: Int? = null,
+    val areaName: String?,
     val industryId: Int? = null,
+    val industryName: String?,
     val salary: Int? = null,
     val onlyWithSalary: Boolean = false
 )
