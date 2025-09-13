f31db7730a27e56e52550a655d89e6de5cfebd07
YanaKudrinskaya <yanakudrinskaya86@gmail.com>
2025-09-13 10:49:31 +0300

    fix detekt

--- app/src/main/java/ru/practicum/android/diploma/ui/filteringsettings/FilteringSettingsFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/filteringsettings/FilteringSettingsFragment.kt
@@ -26,12 +26,12 @@ class FilteringSettingsFragment : Fragment() {
     private val binding get() = _binding!!
     private val viewModel by viewModel<FilterViewModel>()
     private val simpleTextWatcher = object : TextWatcher {
-        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) = Unit
         override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
             binding.clearIcon.visibility = clearButtonVisibility(s)
         }
 
-        override fun afterTextChanged(s: Editable?) {}
+        override fun afterTextChanged(s: Editable?) = Unit
     }
 
     override fun onCreateView(
