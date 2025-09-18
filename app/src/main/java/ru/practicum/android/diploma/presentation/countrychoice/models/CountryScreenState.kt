8a72045a01ae5f64157f735dc5f00c22c5570f07
YanaKudrinskaya <yanakudrinskaya86@gmail.com>
2025-09-18 12:28:06 +0300

    Изменила CountryScreenState на sealed interface

--- app/src/main/java/ru/practicum/android/diploma/presentation/countrychoice/models/CountryScreenState.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/countrychoice/models/CountryScreenState.kt
@@ -2,9 +2,9 @@ package ru.practicum.android.diploma.presentation.countrychoice.models
 
 import ru.practicum.android.diploma.domain.areas.models.Area
 
-sealed class CountryScreenState {
-    object Empty : CountryScreenState()
+sealed interface CountryScreenState {
+    object Empty : CountryScreenState
     data class Content(
         val list: List<Area>
-    ) : CountryScreenState()
+    ) : CountryScreenState
 }
