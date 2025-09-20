829d58e581949ff65d009d65857b834df493e099
YanaKudrinskaya <yanakudrinskaya86@gmail.com>
2025-09-20 09:00:59 +0300

    fix detekt

--- app/src/main/java/ru/practicum/android/diploma/domain/areas/impl/AreasInteractorImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/areas/impl/AreasInteractorImpl.kt
@@ -16,28 +16,24 @@ class AreasInteractorImpl(
 
     override suspend fun getParentArea(areaId: Int): Resource<Area?> {
         val areasResult = repository.getAreas()
-        return when(areasResult) {
+        return when (areasResult) {
             is Resource.Error<*> -> Resource.Error(ResponseStatus.NOT_FOUND)
             is Resource.Success<*> -> findArea(areaId, areasResult.data)
         }
     }
 
     private fun findArea(areaId: Int, areas: List<Area>?): Resource<Area?> {
-        return if(areas != null) {
+        return if (areas != null) {
             Resource.Success(findAreaById(areas, areaId))
-        } else Resource.Error(ResponseStatus.NOT_FOUND)
+        } else {
+            Resource.Error(ResponseStatus.NOT_FOUND)
+        }
     }
 
     private fun findAreaById(areas: List<Area>, targetId: Int): Area? {
-        for (area in areas) {
-            if (area.id == targetId) {
-                return area
-            }
-            val foundInChildren = findAreaById(area.areas, targetId)
-            if (foundInChildren != null) {
-                return foundInChildren
-            }
-        }
-        return null
+        return areas.find { it.id == targetId }
+            ?: areas.asSequence()
+                .mapNotNull { findAreaById(it.areas, targetId) }
+                .firstOrNull()
     }
 }
