c04754c4afa5ddece91e553c3fca4abe14e457fe
ilsdan <me@ilsdan.ru>
2025-09-20 14:47:55 +0200

    Если поле ввода поискового запроса непустое, то в списке присутствуют только те отрасли, названия которых содержат введённый в поле текст

--- app/src/main/java/ru/practicum/android/diploma/ui/industrychoice/IndustryChoiceFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/industrychoice/IndustryChoiceFragment.kt
@@ -5,10 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.isVisible
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentIndustryChoiceBinding
 import ru.practicum.android.diploma.domain.industrychoice.models.Industry
 import ru.practicum.android.diploma.presentation.industrychoice.IndustryChoiceScreenState
@@ -68,6 +70,20 @@ class IndustryChoiceFragment : Fragment() {
         binding.applyButton.setOnClickListener {
             currentIndustryId?.let { viewModel.saveIndustry(it) }
             findNavController().navigateUp()
+        }
+
+        binding.searchFieldIcon.setOnClickListener {
+            binding.searchIndustry.setText("")
+        }
+
+        binding.searchIndustry.doOnTextChanged { text, _, _, _ ->
+            if (binding.searchIndustry.text.toString().isEmpty()) {
+                binding.searchFieldIcon.setImageDrawable(requireContext().getDrawable(R.drawable.ic_search))
+                adapter.filter(null)
+            } else {
+                binding.searchFieldIcon.setImageDrawable(requireContext().getDrawable(R.drawable.ic_close))
+                adapter.filter(binding.searchIndustry.text.toString())
+            }
         }
 
         viewModel.getScreenState().observe(viewLifecycleOwner) { state ->
